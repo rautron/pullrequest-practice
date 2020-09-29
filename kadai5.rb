e2c9d1ce0492b7a772aac5f8313436eed6e5338c
Nguyen Thu Trang <trangntt.uet@gmail.com>
2020-09-29 03:52:45 +0000

    add kadai5

--- kadai5.rb
+++ kadai5.rb
@@ -17,7 +17,10 @@ class Drink < Menu
   
   # initializeメソッドをオーバーライドする
   # nameとpriceはsuperとする
-  
+  def initialize(name:, price:, size:)
+    super(name:name, price:price)
+    self.size = size
+  end
   def info
     return "#{self.name} #{self.price}vnd (#{self.size}サイズ)"
   end
@@ -28,11 +31,19 @@ menu2 = Menu.new(name: "Bún chả", price: 40000)
 menu3 = Menu.new(name: "bánh mì", price: 20000)
 
 # Drinkの変数を定義してください
-
+drink1 = Drink.new(name: "Trà", price: 5000, size:"S")
+drink2 = Drink.new(name: "Trà", price: 10000, size:"M")
 
 # 変数menusを定義して配列を代入してください
+menus = [menu1, menu2, menu3, drink1, drink2]
 
-
-menus.each do |menu|
-  puts "#{menu.info}"
+menus.each.with_index do |menu, index|
+  puts "#{index} .#{menu.info}"
 end
+
+puts "----------------------"
+puts "メニューの番号を選択してください"
+order = gets.to_i
+selected_menu = menus[order]
+puts "選択されたメニュー：#{selected_menu.name}"
+puts "お合計は#{selected_menu.price}vndです"
