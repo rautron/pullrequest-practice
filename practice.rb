01482667b088b3b44afb49e54a645b9e209497ff
Nguyen Thu Trang <trangntt.uet@gmail.com>
2020-09-29 03:17:22 +0000

    add kadai4

--- practice.rb
+++ practice.rb
@@ -2,21 +2,30 @@
 =begin
     print "text \n"
 =end
-puts "text2"
-puts "hello"
+# puts "text2"
+# puts "hello"
 
-name = "conan"
-age = 20
-puts "name#{name}age#{age}"
+# name = "conan"
+# age = 20
+# puts "name#{name}age#{age}"
 
-if age > 50
-    puts "age >50"
-elsif > 30
-    puts "age >30"
-else
-    puts "age is 20"
+# if age > 50
+#     puts "age >50"
+# elsif > 30
+#     puts "age >30"
+# else
+#     puts "age is 20"
+# end
+
+
+
+
+def introduce(name)
+puts "こんにちは"
+puts "私は#{name}です"
 end
-
+#メソッドを呼び出す
+puts introduce("Conan")
 
     
     
