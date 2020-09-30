d55863731cbfdd92bbe36cfb5f7cb518c03d749e
quangdvn <ghitacluber@gmail.com>
2020-09-30 10:19:12 +0000

    add kadai3 kadai4 kadai5

---
+++ kadai3.rb
@@ -0,0 +1,18 @@
+class Menu
+  attr_accessor :name
+  attr_accessor :price
+  
+  # get_total_priceメソッドを定義してください
+  def get_total_price count
+    total_price = count * price
+    total_price -= 10000 unless count < 3
+    "#{name} #{total_price} VND."
+  end
+end
+
+menu1 = Menu.new
+menu1.name = "Phở"
+menu1.price = 30000
+puts menu1.get_total_price 5
+
+# menu1に対してget_total_priceメソッドを呼び出してください
---
+++ kadai5.rb
@@ -0,0 +1,55 @@
+class Menu
+  attr_accessor :name
+  attr_accessor :price
+  
+  def initialize name:, price:
+    self.name = name
+    self.price = price
+  end
+  
+  def info
+    "#{name} #{price}VND"
+  end
+end
+
+class Drink < Menu
+  attr_accessor :size
+  
+  # initializeメソッドをオーバーライドする
+  # nameとpriceはsuperとする
+  def initialize name:, price:, size:
+    super name: name, price: price
+    self.size = size
+  end
+  
+  def info
+    "#{name} #{price}VND (#{size}サイズ)"
+  end
+end
+
+menu1 = Menu.new name: "Phở", price: 30000
+menu2 = Menu.new name: "Bún chả", price: 40000
+menu3 = Menu.new name: "bánh mì", price: 20000
+menu4 = Drink.new name: "Milo", price: 5000, size: "M"
+menu5 = Drink.new name: "Nestea", price: 10000, size: "S"
+
+# Drinkの変数を定義してください
+menus = [menu1, menu2, menu3, menu4, menu5]
+
+# 変数menusを定義して配列を代入してください
+menus.each_with_index do |menu, index|
+  puts "#{index + 1}. #{menu.info}"
+end
+
+puts "Choose your menu: "
+
+order = gets.to_i()
+
+if menus[order - 1] == nil 
+  return puts "Unavailable"
+end
+
+selected_menu = menus[order - 1]
+
+puts "選択されたメニュー: #{selected_menu.name}"
+puts "お会計は #{selected_menu.price}VND"
