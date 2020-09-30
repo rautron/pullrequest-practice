b3bd2f6a373447d874417bbcab2bc79d543b9c76
EC2 Default User <huongtrauet@gmail.com>
2020-09-30 10:23:29 +0000

    kadai3_4_5

--- kadai4.rb
+++ kadai4.rb
@@ -13,9 +13,15 @@ class Menu
 end
 
 #メニューの変数を定義してください
-menu1 = 
-menu2 = 
-menu3 = 
+menu1 = Menu.new(name: "Phở",price: 30000)
+menu2 = Menu.new(name: "Bún chả",price: 40000)
+menu3 = Menu.new(name: "Bánh mỳ",price: 20000)
+
+menus = [menu1, menu2, menu3]
+
+menus.each do |m|
+  puts m.info
+end
 
 # 変数menusを定義して配列を代入してください
 
